b069ae5b53e23706cc33f6d478628ffd0699322d
auliyaardhinii <162085633+auliyaardhinii@users.noreply.github.com>
2025-05-21 16:01:54 +0700

    APLIKASI PENCARIAN DAN PEMBELIAN KONSER NCT DREAM.py

--- APLIKASI PENCARIAN DAN PEMBELIAN KONSER NCT DREAM.py
+++ APLIKASI PENCARIAN DAN PEMBELIAN KONSER NCT DREAM.py
@@ -12,7 +12,8 @@ import uuid
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_excel("c:\\Users\\auliy\\Downloads\\NCT_DREAM_KONSER.xlsx")
+    df = pd.read_excel("data/NCT_DREAM_KONSER.xlsx")
+")
     return df
 
 df = load_data()
